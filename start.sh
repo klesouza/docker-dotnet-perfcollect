97a6af8476314bcd70b8da2933c27687c152280a
Kleber <kle_zoio@hotmail.com>
2018-01-02 20:23:55 +0100

    adding env variables to dotnet run

--- start.sh
+++ start.sh
@@ -4,6 +4,8 @@ docker build . -f Dockerfile -t ksilva/dotnet-trace
 docker rm dotnet-trace -f || true && \
 docker run -d -p 5000:5000 \
     -v $(pwd)/data:/tmp/prof:rw \
+    -e "COMPlus_PerfMapEnabled=1" \
+    -e "COMPlus_EnableEventLog=1" \
     --security-opt seccomp=$(pwd)/seccomp.json \
     --name dotnet-trace  \
     ksilva/dotnet-trace
